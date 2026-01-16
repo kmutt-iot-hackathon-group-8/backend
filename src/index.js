1db8949c792d558dce4f3b7022184b867db89c19
Mykal-Steele <mykal.stele@gmail.com>
2026-01-16 21:50:19 +0700

    feat: add health check

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+  })
 );
 
 // Auth routes
@@ -73,7 +73,7 @@ app.post("/api/v1/register-user", (req, res) => {
 
 // Home page
 app.get("/", (_, res) => {
-  res.sendFile(join(__filename, "index.html"));
+  res.json({ success: true, message: "ok" });
 });
 
 // ===== SOCKET.IO EVENTS =====
