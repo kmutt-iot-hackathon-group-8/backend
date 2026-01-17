ffdd19d7b45af37fd2eb0438ac6486e53f635034
Nicha-h <nichahongsri@gmail.com>
2026-01-17 21:25:49 +0700

    feat: delete

--- src/routes/event.route.js
+++ src/routes/event.route.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const multer = require('multer');
-const { createEvent, getAllUserEvents } = require('../controller/event.controller');
+const { createEvent, getAllUserEvents, updateEvent, deleteEvent, getEventById } = require('../controller/event.controller');
 
 const upload = multer();
 const router = express.Router();
 router.post('/event', upload.none(), createEvent);
-router.get('/event/attended/:userId', upload.none(), getAllUserEvents)
+router.put('/event/:eventId', upload.none(), updateEvent);
+router.delete('/event/:eventid', upload.none(), deleteEvent);
+router.get('/event/:eventId', upload.none(), getEventById);
+router.get('/event/attended/:userId', upload.none(), getAllUserEvents);
 module.exports = router;
